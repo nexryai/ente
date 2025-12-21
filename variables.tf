dd3dea69de8d5d1a3d0a53ad63502bf3b5d705c5
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-21 18:00:38 +0900

    Add Terraform variables for project configuration

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "region" {
+  default = "us-west1"
+}
+
+variable "zone" {
+  default = "us-west1-a"
+}
