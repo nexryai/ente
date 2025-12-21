91e2ff6f9572388d3a70f9a25db12db2ea08a154
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-21 20:10:33 +0900

    Enable private IP Google access for subnet
    Added private IP Google access to the subnet resource.

--- main.tf
+++ main.tf
@@ -81,10 +81,11 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "ente-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.vpc.id
-  region        = var.region
+  name                     = "ente-subnet"
+  ip_cidr_range            = "10.0.1.0/24"
+  network                  = google_compute_network.vpc.id
+  region                   = var.region
+  private_ip_google_access = true
 }
 
 # --- Artifact Registry ---
